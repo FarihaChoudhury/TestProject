6aae74f4030d45e78122357e468ddfca1ce50136
rufia1 <129317917+rufia1@users.noreply.github.com>
2023-03-29 19:18:14 +0100

    Update forms.py

--- microblogs/forms.py
+++ microblogs/forms.py
@@ -9,3 +9,18 @@ class SignUpForm(forms.ModelForm):
 
     new_password = forms.CharField(label='Password', widget=forms.PasswordInput())
     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+    
+    
+ 
+class PostForm(forms.ModelForm):
+ """Form to ask user for post text.
+The post author must be by the post creator."""
+
+    class Meta:
+        """Form options."""
+
+        model = Post
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea()
+        }
